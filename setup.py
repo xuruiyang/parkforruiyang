9f335b26ca5ef9d8cdf9f2b58aff9925fea8c5f8
Ruiyang Xu <xuruiyang@users.noreply.github.com>
2020-10-22 02:03:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='parkforruiyang',
       packages = find_packages(),
-      version='0.0.7',
+      version='0.0.8',
       install_requires=['numpy', 'wget', 'networkx', 'gym', 'dill', 'pycapnp', 'pandas'],
       author = 'Ruiyang',                   # Type in your name
       author_email = 'ruiyang@ccs.neu.edu',      # Type in your E-Mail
       url = 'https://github.com/xuruiyang/parkforruiyang',   # Provide either the link to your github or to your website
-      download_url = 'https://github.com/xuruiyang/parkforruiyang/archive/0.0.7.tar.gz',    # I explain this later on
+      download_url = 'https://github.com/xuruiyang/parkforruiyang/archive/0.0.8.tar.gz',    # I explain this later on
 )
